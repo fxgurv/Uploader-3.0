0ed7264298d07f1d261422e1c9cf42c14c966be1
Strange <168190107+fxgurv@users.noreply.github.com>
2024-11-01 00:44:11 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,45 @@
+class Config:
+    # YouTube
+    youtube_email = "your_email"
+    youtube_password = "your_password"
+    youtube_cookies_file = "youtube_cookies.pkl"
+    
+    # TikTok
+    tiktok_username = "your_username"
+    tiktok_password = "your_password"
+    tiktok_cookies_file = "tiktok_cookies.pkl"
+    
+    # Instagram
+    instagram_username = "your_username"
+    instagram_password = "your_password"
+    instagram_cookies_file = "instagram_cookies.pkl"
+    
+    # Facebook
+    facebook_email = "your_email"
+    facebook_password = "your_password"
+    facebook_cookies_file = "facebook_cookies.pkl"
+    
+    # Reddit
+    reddit_username = "your_username"
+    reddit_password = "your_password"
+    reddit_cookies_file = "reddit_cookies.pkl"
+    
+    # Twitch
+    twitch_username = "your_username"
+    twitch_password = "your_password"
+    twitch_cookies_file = "twitch_cookies.pkl"
+    
+    # Pinterest
+    pinterest_email = "your_email"
+    pinterest_password = "your_password"
+    pinterest_cookies_file = "pinterest_cookies.pkl"
+    
+    # Snapchat
+    snapchat_username = "your_username"
+    snapchat_password = "your_password"
+    snapchat_cookies_file = "snapchat_cookies.pkl"
+    
+    # Twitter
+    twitter_username = "your_username"
+    twitter_password = "your_password"
+    twitter_cookies_file = "twitter_cookies.pkl"
